780da03b8bf82a4ccb05045b1762bb9fe347847f
Ali HAMDI <ahamdi@exoplatform.com>
2025-02-24 16:09:09 +0100

    fix: adjust chat notification badge size - meed-8457 - Meeds-io/MIPs-163 (#54)
    Adjust the notification badge size and add a color to the user names when in a space room discussion

--- services/src/main/java/io/meeds/chat/rest/model/RoomEntity.java
+++ services/src/main/java/io/meeds/chat/rest/model/RoomEntity.java
@@ -49,6 +49,8 @@ public class RoomEntity implements Serializable {
 
   private String       userId;
 
+  private String       identityId;
+
   private String       spaceId;
 
   private boolean      favorite;
