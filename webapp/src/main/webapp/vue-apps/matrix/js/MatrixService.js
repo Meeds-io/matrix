1c513656ae9734b2df228fb9c2fe03c10f26923d
Ali Hamdi <ahamdi@exoplatform.com>
2025-04-09 16:15:57 +0100

    feat: enable mentioning users in chat - MEED-8514 - Meeds-io/MIPs#163

--- webapp/src/main/webapp/vue-apps/matrix/js/MatrixService.js
+++ webapp/src/main/webapp/vue-apps/matrix/js/MatrixService.js
@@ -213,7 +213,9 @@ export function processEvents(response) {
         //message received in a room
         if(e.type === 'm.room.message') {
           if(e.content.msgtype === 'm.text') {
-            document.dispatchEvent(new CustomEvent('matrix-message-received', { detail: {roomId: roomId, sender: e.sender, message: e.content.body, origin_server_ts: e.origin_server_ts, event_id : e.event_id}}));
+            const messageContent = e.content.format === 'org.matrix.custom.html' && e.content.formatted_body || e.content.body;
+            const messageText = e.content.format === 'org.matrix.custom.html' && formatMentionsInRoomList(e.content.formatted_body) || e.content.body;
+            document.dispatchEvent(new CustomEvent('matrix-message-received', { detail: {roomId: roomId, sender: e.sender, message: messageContent, messageText: messageText, origin_server_ts: e.origin_server_ts, event_id : e.event_id}}));
           }
         } // Joined a new room
         else if(e.type === 'm.room.member') {
@@ -315,7 +317,7 @@ export async function toRoomObject(rooms, currentMemberId) {
         if(e.content.msgtype === 'm.text' && (!roomItem.updated || roomItem.updated <= e.origin_server_ts)) {
           roomItem.updated = e.origin_server_ts;
           roomItem.lastMessage = {};
-          roomItem.lastMessage.content = e.content.body;
+          roomItem.lastMessage.content = e.content.format === 'org.matrix.custom.html' && formatMentionsInRoomList(e.content.formatted_body) || e.content.body;
           roomItem.lastMessage.sender = e.sender;
           roomItem.lastMessage.eventId = e.event_id;
         }
@@ -588,7 +590,6 @@ export function getUserPresence(userIdOnMatrix) {
     }).then(status => {
       return status.presence;
     }).catch(e => {
-      console.error(e);
       return 'offline';
     });
 }
@@ -734,14 +735,10 @@ export function getUserDisplayNameFontColor(identityId) {
   return `color: ${colors[Number(identityId) % colors.length]} !important`;
 }
 
-export function sendMessage(message, roomId) {
+export function sendMessage(payload, roomId) {
   let index = localStorage.getItem('matrix_transaction_index') || 1;
   const transactionId = `${new Date().getTime()}-${index}`;
   const eventType = 'm.room.message';
-  const payload = {
-                    'body': message,
-                    'msgtype': 'm.text'
-                  };
   return fetch(`/_matrix/client/v3/rooms/${roomId}/send/${eventType}/${transactionId}`, {
     method: 'PUT',
     headers: {
@@ -787,4 +784,13 @@ export function markRoomAsFullyRead(roomId, eventId) {
       return true;
     }
   });
+}
+
+export function formatMentionsInMessage(message) {
+  return message.replace(/<a href=\"https:\/\/matrix\.to\/#\/([^"]+)\">([^"]+)<\/a>/g, '<a href=\"\/matrix\/rest\/matrix\/profile\/$1\" class=\"font-weight-bold\" target=\"_blank\">@$2<\/a>')
+                      .replace(/\n/g, '<br />') || '';
+}
+export function formatMentionsInRoomList(message) {
+  return message.replace(/<a href=\"https:\/\/matrix\.to\/#\/([^"]+)\">([^"]+)<\/a>/g, '<span class=\"font-weight-bold\" target=\"_blank\">@$2<\/span>')
+                      .replace(/\n/g, '<br />') || '';
 }
