5f360dc6428b4f3fdb7a4e916a848b3a5744dfdf
Helmi Akermi <hakermi@exoplatform.com>
2025-05-05 14:18:30 +0100

    feat: Display reply to a message in chat drawer - MEED-8680 - Meeds-io/MIPs#163
    Display reply to a message in chat drawer

---
+++ webapp/src/main/webapp/vue-apps/matrix/components/message/MessageReplyQuote.vue
@@ -0,0 +1,146 @@
+<!--
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2025 Meeds Association contact@meeds.io
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+
+<template>
+  <v-card
+    class="white pa-2 border-radius-16 clickable"
+    flat
+    @click="goToMessageSource">
+    <div class="d-flex flex-row">
+      <div
+        v-if="!isText"
+        class="flex-column justify-center">
+        <div
+          v-if="isFile || isAudio"
+          class="size-7 background-grey-primary rounded-circle d-flex justify-center me-2">
+          <v-icon
+            :size="16"
+            :color="isFile && fileIcon.color">
+            {{ isFile && fileIcon.class || 'fas fa-microphone' }}
+          </v-icon>
+        </div>
+        <div v-else>
+          <v-img
+            :src="imageThumbnailURL"
+            :max-height="36"
+            :max-width="36"
+            :aspect-ratio="thumbnailAspectRatio"
+            contain>
+          </v-img>
+        </div>
+      </div>
+      <div class="flex-column">
+        <div class="flex-row">
+          <quoted-message-user :user-id="targetMessage.targetUser" />
+        </div>
+        <v-sheet
+          :max-width="isFile && 265 || 'auto'"
+          class="flex-row">
+          <div
+            :class="{'text-truncate-3': isText, 'text-truncate': isFile}"
+            class="text-color">
+            {{ targetMessageBody }}
+          </div>
+        </v-sheet>
+      </div>
+    </div>
+  </v-card>
+
+</template>
+
+<script>
+
+export default {
+  props: {
+    message: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    targetMessage() {
+      return this.message?.replyTo;
+    },
+    targetMessageBody() {
+      return this.targetMessage.body;
+    },
+    targetMessageType() {
+      return this.targetMessage?.targetType;
+    },
+    isText() {
+      return this.targetMessageType === 'm.text';
+    },
+    isAudio() {
+      return this.targetMessageType === 'm.audio';
+    },
+    isFile() {
+      return this.targetMessageType === 'm.file';
+    },
+    thumbnailWidth() {
+      return this.targetMessage?.content?.info?.w || this.targetMessage.content?.w;
+    },
+    thumbnailHeight() {
+      return this.targetMessage?.content?.info?.h || this.targetMessage.content?.h;
+    },
+    thumbnailURL() {
+      return this.targetMessage?.content?.thumbnail_url;
+    },
+    url() {
+      return this.targetMessage?.content?.url;
+    },
+    thumbnailAspectRatio() {
+      return this.thumbnailWidth / this.thumbnailHeight;
+    },
+    imageThumbnailURL() {
+      const url = this.thumbnailURL || this.url;
+      if (!url) {
+        return null;
+      }
+      const imageId = url.replace(`mxc://${matrixServerName}/`, '');
+      const basePath = this.thumbnailURL ? `/_matrix/media/v3/thumbnail` : `/_matrix/media/v3/download`;
+      const params = this.thumbnailURL ? `?width=800&height=600&method=scale&allow_redirect=true` : `?allow_redirect=true`;
+
+      return `${basePath}/${matrixServerName}/${imageId}${params}`;
+    },
+    fileMimeType() {
+      return this.targetMessage?.fileMimeType;
+    },
+    fileIcon() {
+      return this.isFile && this.getFileIcon(this.fileMimeType);
+    }
+  },
+  methods: {
+    goToMessageSource() {
+      const targetId = `message-content-${this.targetMessage.targetEventId}`;
+      const targetElement = document.getElementById(targetId).parentElement;
+      if (targetElement) {
+        targetElement.scrollIntoView({behavior: 'smooth', block: 'end'});
+      }
+    },
+    getFileIcon(mimeType) {
+      const extensions = Vue.prototype.$filesIconsExtension;
+      let extension = extensions[0].get(mimeType);
+      if (!extension) {
+        extension = extensions[0].get('file');
+      }
+      return extension;
+    }
+  }
+};
+</script>
--- webapp/src/main/webapp/vue-apps/matrix/js/MatrixService.js
+++ webapp/src/main/webapp/vue-apps/matrix/js/MatrixService.js
@@ -1,6 +1,9 @@
 import {chatConstants} from './Constants.js';
 import * as timeUtils from './timeUtils.js';
 
+const replyToCache = new Map();
+const userCache = new Map();
+
 // variables that will be get from the server
 const MATRIX_SERVER_URL='http://localhost:8008';
 const JWT_COOKIE_NAME = 'matrix_jwt_token';
@@ -629,18 +632,25 @@ export function getUserPresence(userIdOnMatrix) {
 
 export function getUserByMatrixId(userIdOnMatrix) {
   let senderMatrixId = userIdOnMatrix;
-  if(senderMatrixId.includes('@')) {
+  if (senderMatrixId.includes('@')) {
     senderMatrixId = userIdOnMatrix.substr(1, userIdOnMatrix.indexOf(":") - 1);
   }
+
+  if (userCache.has(senderMatrixId)) {
+    return Promise.resolve(userCache.get(senderMatrixId));
+  }
+
   return fetch(`/matrix/rest/matrix/userByMatrixId?userMatrixId=${senderMatrixId}`, {
     method: 'GET',
     credentials: 'include',
-  },).then(resp => {
+  }).then(resp => {
     if (!resp || !resp.ok) {
-      throw new Error('Get User by Matrix ID : Response code indicates a server error', resp);
-    } else {
-      return resp.json();
+      throw new Error('Get User by Matrix ID : Response code indicates a server error');
     }
+    return resp.json();
+  }).then(user => {
+    userCache.set(senderMatrixId, user);
+    return user;
   });
 }
 
@@ -846,9 +856,19 @@ export function processMessages(messageItems) {
           originalMessage.content.msgtype = newContent.msgtype || originalMessage.content.msgtype;
           originalMessage.edited = true;
           originalMessage.updatedAt = item.origin_server_ts;
+
+          for (const message of messagesMap.values()) {
+            if (message?.replyTo?.targetEventId === targetEventId) {
+              message.replyTo = buildReplyToObject(messagesMap, message.replyTo.targetEventId);
+            }
+          }
         }
       } else {
         item.reactions = item.reactions || [];
+        const inReplyTo = relatesTo?.['m.in_reply_to']?.event_id;
+        if (inReplyTo) {
+          item.replyTo = buildReplyToObject(messagesMap, inReplyTo);
+        }
         messagesMap.set(item.event_id, item);
       }
     } else if (item.type === 'm.reaction') {
@@ -879,3 +899,78 @@ export function processMessageReaction(messageReactedTo, reactionItem) {
   messageReactedTo.reactions = Array.from(messageReactedTo.reactionsMap.values());
   return messageReactedTo;
 }
+
+export function buildReplyToObject(messages, eventId) {
+  const getMessageById = (id) =>
+      (messages instanceof Map)
+          ? messages.get(id)
+          : (Array.isArray(messages) ? messages.find(msg => msg.event_id === id) : null);
+
+  const parentEvent = getMessageById(eventId);
+  if (!parentEvent) {
+    return null;
+  }
+
+  const cachedReplyTo = replyToCache.get(eventId);
+  const parentUpdatedAt = parentEvent.updatedAt || parentEvent.origin_server_ts;
+
+  if (cachedReplyTo && cachedReplyTo.updatedAt === parentUpdatedAt) {
+    return cachedReplyTo.replyTo;
+  }
+
+  const parentRelatesTo = parentEvent.content?.['m.relates_to'];
+  const isReplyToReply = !!parentRelatesTo?.['m.in_reply_to'];
+
+  let targetUser = parentEvent.sender;
+  let targetEventId = parentEvent.event_id;
+  let ancestor = parentEvent;
+  let targetType = parentEvent.content?.msgtype
+  let targetThumbnailURL = parentEvent.content?.thumbnail_url;
+  let TargetUrl = parentEvent.content?.url;
+  let targetThumbnailWidth = parentEvent.content?.info?.w || parentEvent.content?.w;
+  let targetThumbnailHeight = parentEvent.content?.info?.h || parentEvent.content?.h
+  let fileMimeType = parentEvent.content?.info?.mimetype;
+
+  const visited = new Set();
+
+  while (ancestor.content?.['m.relates_to']?.['m.in_reply_to']?.event_id) {
+    const ancestorEventId = ancestor.content['m.relates_to']['m.in_reply_to'].event_id;
+
+    if (visited.has(ancestorEventId)) {
+      break;
+    }
+    visited.add(ancestorEventId);
+
+    const ancestorEvent = getMessageById(ancestorEventId);
+    if (!ancestorEvent) {
+      break;
+    }
+
+    targetUser = ancestorEvent.sender;
+    targetEventId = ancestorEvent.event_id;
+    ancestor = ancestorEvent;
+    targetType = ancestorEvent.content?.msgtype;
+    targetThumbnailURL = ancestor.content?.thumbnail_url;
+    TargetUrl = ancestor.content?.url;
+    targetThumbnailWidth = ancestor.content?.info?.w || ancestor.content?.w;
+    targetThumbnailHeight = ancestor.content?.info?.h || ancestor.content?.h
+    fileMimeType = ancestor.content?.info?.mimetype;
+  }
+
+  const replyToObject = {
+    body: parentEvent.content.body,
+    isReplyToReply: isReplyToReply,
+    targetUser: targetUser,
+    targetEventId: targetEventId,
+    targetType: targetType,
+    targetThumbnailURL: targetThumbnailURL,
+    targetUrl: TargetUrl,
+    targetThumbnailWidth: targetThumbnailWidth,
+    targetThumbnailHeight: targetThumbnailHeight,
+    fileMimeType: fileMimeType
+  };
+
+  replyToCache.set(eventId, { replyTo: replyToObject, updatedAt: parentUpdatedAt });
+  return replyToObject;
+}
+
