62847fb06982554dc3e5f9ccee24fc24327d53e7
Ali Hamdi <ahamdi@exoplatform.com>
2025-05-16 15:37:16 +0100

    fix: replace Matrix unallowed chars with hyphen character - MEED-9102 - Meeds-io/MIPs#163

--- services/src/main/java/io/meeds/chat/service/MatrixService.java
+++ services/src/main/java/io/meeds/chat/service/MatrixService.java
@@ -217,6 +217,7 @@ public class MatrixService {
    */
   public String getJWTSessionToken(String userNameOnMatrix) {
     Date expirtaionDate = Date.from(Instant.now().plusSeconds(7 * 24 * 60 * 60L)); // adds one week to the current instant
+    userNameOnMatrix = userNameOnMatrix.replaceAll("[^a-zA-Z=_\\-\\.\\/+]+", "-");
     return Jwts.builder()
                .setSubject(userNameOnMatrix)
                .signWith(Keys.hmacShaKeyFor(PropertyManager.getProperty(MATRIX_JWT_SECRET).getBytes()))
