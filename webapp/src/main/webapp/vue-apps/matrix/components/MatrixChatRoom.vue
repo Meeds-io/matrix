77ada4233bb14781296f171fad14a3f0d4b2f4f7
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2025-05-19 12:52:40 +0100

    feat: Add hover effect on discussion list items - MEED-8475 - Meeds-io/MIPs#163 (#199)
    Add hover effect on discussion list items

--- webapp/src/main/webapp/vue-apps/matrix/components/MatrixChatRoom.vue
+++ webapp/src/main/webapp/vue-apps/matrix/components/MatrixChatRoom.vue
@@ -1,41 +1,45 @@
 <template>
-  <div class="d-flex chat-room-item">
+  <v-hover v-slot="{ hover }">
     <div
-      :style="`backgroundImage: url(${room.avatarUrl})`"
-      :class="avatarBorderClass"
-      class="meeds-chat-contact-avatar no-border size-13 d-flex">
-      <div v-if="room.directChat" class="matrix-user-status size-3"
-        :class="presenceClass"></div>
-    </div>
-    <div class="clickable overflow-hidden ps-2 flex-grow-1"
-       @click="openRoom">
-      <div :id="`room-name-${room.id}`"
-        class="chat-room-name text-truncate text-title text-subtitle-1"
-        :style="roomNameStyle">
-        {{ room.name }} <span v-if="room.external">{{ externalTag }}</span>
-      </div>
+      :class="{'background-grey-primary': hover}"
+      class="d-flex chat-room-item py-3 px-5 clickable">
       <div
-        v-if="room.lastMessage"
-        class="chat-room-last-message text-truncate mt-1"
-        :class="lastMessageStyle"
-        v-sanitized-html="room.lastMessage.content">
+        :style="`backgroundImage: url(${room.avatarUrl})`"
+        :class="avatarBorderClass"
+        class="meeds-chat-contact-avatar no-border size-13 d-flex">
+        <div v-if="room.directChat" class="matrix-user-status size-3"
+          :class="presenceClass"></div>
       </div>
-      <div v-else class="text-subtitle text-truncate mt-1">
-        {{ $t('matrix.chat.start.conversation') }}
+      <div class="clickable overflow-hidden ps-2 flex-grow-1"
+         @click="openRoom">
+        <div :id="`room-name-${room.id}`"
+          class="chat-room-name text-truncate text-title text-subtitle-1"
+          :style="roomNameStyle">
+          {{ room.name }} <span v-if="room.external">{{ externalTag }}</span>
+        </div>
+        <div
+          v-if="room.lastMessage"
+          class="chat-room-last-message text-truncate mt-1"
+          :class="lastMessageStyle"
+          v-sanitized-html="room.lastMessage.content">
+        </div>
+        <div v-else class="text-subtitle text-truncate mt-1">
+          {{ $t('matrix.chat.start.conversation') }}
+        </div>
       </div>
-    </div>
-    <div class="ps-3">
-      <div class="last-message-timestamp text-subtitle">
-        {{ getUpdateTime(room) }}
-      </div>
-      <div class="pull-right text-font-small-size d-flex">
-        <v-icon v-if="room.isMuted" size="16">fas fa-bell-slash</v-icon>
-        <div v-if="room.unreadMessages" class="unread-messages align-center border-radius-circle error-color-background white--text text-font-small-size align-content-center">
-          {{ room.unreadMessages <= 99 ? room.unreadMessages : '99+' }}
+      <div class="ps-3">
+        <div class="last-message-timestamp text-subtitle">
+          {{ getUpdateTime(room) }}
+        </div>
+        <div class="pull-right text-font-small-size d-flex">
+          <v-icon v-if="room.isMuted" size="16">fas fa-bell-slash</v-icon>
+          <div v-if="room.unreadMessages" class="unread-messages align-center border-radius-circle error-color-background white--text text-font-small-size align-content-center">
+            {{ room.unreadMessages <= 99 ? room.unreadMessages : '99+' }}
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </v-hover>
 </template>
 <script>
   export default {
