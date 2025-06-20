1818f11d5418341fc80dc06f25ca285817bff774
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2025-06-20 12:02:17 +0100

    feat: Allow open discussion by clicking on the whole area of room list item - MEED-9231 - Meeds-io/MIPs#164 (#248)
    Allow open discussion by clicking on the whole area of room list item

--- webapp/src/main/webapp/vue-apps/matrix/components/MatrixChatRoom.vue
+++ webapp/src/main/webapp/vue-apps/matrix/components/MatrixChatRoom.vue
@@ -2,19 +2,25 @@
   <v-hover v-slot="{ hover }">
     <div
       :class="{'background-grey-primary': hover}"
-      class="d-flex chat-room-item py-3 px-5 clickable">
+      class="d-flex chat-room-item py-3 px-5 clickable"
+      @click="openRoom">
       <div
         :style="`backgroundImage: url(${room.avatarUrl})`"
         :class="avatarBorderClass"
         class="meeds-chat-contact-avatar no-border size-13 d-flex">
-        <div v-if="room.directChat" class="matrix-user-status size-3"
-          :class="presenceClass"></div>
+        <div
+          v-if="room.directChat"
+          :class="presenceClass"
+          class="matrix-user-status size-3" />
       </div>
-      <div class="clickable overflow-hidden ps-2 flex-grow-1"
-         @click="openRoom">
-        <div :id="`room-name-${room.id}`"
+      <div class="overflow-hidden ps-2 flex-grow-1">
+        <div
+          :id="`room-name-${room.id}`"
           class="chat-room-name text-truncate text-title text-subtitle-1">
-          {{ room.name }} <span v-if="room.external">{{ externalTag }}</span>
+          {{ room.name }}
+          <span v-if="room.external">
+            {{ externalTag }}
+          </span>
         </div>
         <room-last-message :room="room" />
       </div>
@@ -23,8 +29,15 @@
           {{ getUpdateTime(room) }}
         </div>
         <div class="pull-right text-font-small-size d-flex">
-          <v-icon v-if="room.isMuted" size="16">fas fa-bell-slash</v-icon>
-          <div v-if="room.unreadMessages" class="unread-messages align-center border-radius-circle error-color-background white--text text-font-small-size align-content-center">
+          <v-icon
+            v-if="room.isMuted"
+            size="16">
+            fas fa-bell-slash
+          </v-icon>
+          <div
+            v-if="room.unreadMessages"
+            class="unread-messages align-center border-radius-circle error-color-background
+            white--text text-font-small-size align-content-center">
             {{ room.unreadMessages <= 99 ? room.unreadMessages : '99+' }}
           </div>
         </div>
