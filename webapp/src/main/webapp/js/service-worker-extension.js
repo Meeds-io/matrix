6818776414fa80aa113626388bca4d6ebaa481fb
Ali Hamdi <ahamdi@exoplatform.com>
2025-07-22 22:30:31 +0100

    feat: enable Push notification for new messages - MEED-9270 - meeds-io/Mips#189

--- webapp/src/main/webapp/js/service-worker-extension.js
+++ webapp/src/main/webapp/js/service-worker-extension.js
@@ -13,16 +13,11 @@ self.addEventListener('push', event => {
         event.waitUntil(new Promise(async (resolve, reject) => {
           try {
             if (action === 'open') {
-              console.log('service worker extension START');
-              console.log('room ID is : ',roomId);
-              console.log('Event ID is ', eventId);
-              console.log('Access token is : ', accessToken);
               let chatNotification = await fetch(`/matrix/rest/matrix/notification/${roomId}/${eventId}`, {
                 method: 'PUT',
                 credentials: 'include',
                 body: accessToken
               }).then(resp => resp.ok && resp.json());
-              console.log('The retrieved notification is ', chatNotification);
               if (chatNotification) {
                 const title = chatNotification.title;
                 chatNotification.icon = self.location.origin + chatNotification.icon;
@@ -30,7 +25,6 @@ self.addEventListener('push', event => {
                 await self.registration.showNotification(title, chatNotification);
                 await refreshBadge();
               }
-              console.log('service worker extension END');
             }
           } catch (e) {
             reject(e);
@@ -42,8 +36,6 @@ self.addEventListener('push', event => {
 });
 
 self.addEventListener('message', event => {
-  console.log('📩 Message received in service worker:', event.data);
-
   if (event.data.action === 'matrix_access_token') {
     accessToken = event.data.value;
   }
