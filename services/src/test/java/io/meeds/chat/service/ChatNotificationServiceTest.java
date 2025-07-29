8a88b51e181ed245bcb4a32bcef6f49264b7d596
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2025-07-29 13:20:53 +0100

    feat: Chat push notif accroding to user status - MEED-9469 - Meeds-io/MIPs#188 (#284)
    Chat push notif accroding to user status

--- services/src/test/java/io/meeds/chat/service/ChatNotificationServiceTest.java
+++ services/src/test/java/io/meeds/chat/service/ChatNotificationServiceTest.java
@@ -5,24 +5,27 @@ import io.meeds.chat.model.MatrixMessage;
 import io.meeds.chat.model.Room;
 import io.meeds.pwa.model.PwaNotificationMessage;
 import io.meeds.pwa.service.PwaNotificationService;
+import org.exoplatform.commons.utils.CommonsUtils;
 import org.exoplatform.services.resources.LocaleConfig;
 import org.exoplatform.services.resources.Orientation;
 import org.exoplatform.services.resources.impl.LocaleConfigImpl;
+import org.exoplatform.services.user.UserStateModel;
+import org.exoplatform.services.user.UserStateService;
 import org.exoplatform.social.core.identity.model.Identity;
 import org.exoplatform.social.core.manager.IdentityManager;
 import org.exoplatform.social.core.space.model.Space;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Locale;
 import java.util.concurrent.ScheduledFuture;
 
-import static io.meeds.pwa.service.PwaNotificationService.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -30,25 +33,61 @@ import static org.mockito.Mockito.*;
 class ChatNotificationServiceTest extends MatrixBaseTest {
 
   @Autowired
-  MatrixService           matrixService;
+  MatrixService                      matrixService;
 
   @Autowired
-  IdentityManager         identityManager;
+  IdentityManager                    identityManager;
 
   @Autowired
-  ChatNotificationService chatNotificationService;
+  ChatNotificationService            chatNotificationService;
 
   @Autowired
-  PwaNotificationService  pwaNotificationService;
+  PwaNotificationService             pwaNotificationService;
+
+  @Mock
+  private UserStateService           userStateService;
+
+  @Mock
+  private UserStateModel             userStateModel;
+
+  private MockedStatic<CommonsUtils> commonsUtils;
+
+
+  @BeforeEach
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    commonsUtils = mockStatic(CommonsUtils.class, CALLS_REAL_METHODS);
+    commonsUtils.when(() -> CommonsUtils.getService(UserStateService.class)).thenReturn(userStateService);
+
+    when(userStateService.getUserState(anyString())).thenReturn(userStateModel);
+  }
+
+  @AfterEach
+  @Override
+  public void tearDown() {
+    if (commonsUtils != null) {
+      commonsUtils.close();
+    }
+    super.tearDown();
+  }
 
   @Test
   void sendCreateNotificationAction() {
-    ScheduledFuture<?> action = chatNotificationService.sendCreateNotificationAction("eventIDOnMatrix", "demo", "!roomId:matrix.meeds.tn", 5);
+    when(userStateModel.getStatus()).thenReturn("available");
+    ScheduledFuture<?> action = chatNotificationService.sendCreateNotificationAction("eventIDOnMatrix",
+                                                                                     "demo",
+                                                                                     "!roomId:matrix.meeds.tn",
+                                                                                     5);
     assertNotNull(action);
+    when(userStateModel.getStatus()).thenReturn("donotdisturb");
+    action = chatNotificationService.sendCreateNotificationAction("eventIDOnMatrix", "demo", "!roomId:matrix.meeds.tn", 5);
+    assertNull(action);
   }
 
   @Test
   void createNotification() throws Exception {
+    when(userStateModel.getStatus()).thenReturn("available");
     String eventId = "eventIDOnMatrix";
     Space space = getSpaceInstance(1);
     String roomId = matrixService.createRoom(space);
