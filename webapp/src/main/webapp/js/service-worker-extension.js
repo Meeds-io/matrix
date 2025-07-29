74dbbfcd046b4953d63c68162862f2da65ad8797
Ali HAMDI <ahamdi@exoplatform.com>
2025-07-29 13:18:05 +0100

    feat: enhance retrieving user token - MEED-9270 - Meeds-io/MIPs#189 (#285)

--- webapp/src/main/webapp/js/service-worker-extension.js
+++ webapp/src/main/webapp/js/service-worker-extension.js
@@ -1,11 +1,12 @@
 let accessToken = null;
+const CHAT_NOTIFICATION_TYPE = 'CHAT_NOTIFICATION';
 self.addEventListener('push', event => {
   if (self?.Notification?.permission === 'granted') {
     const data = event?.data?.text?.() || {};
     const params = data.split(':');
     if(params.length == 3) {
       const notificationType = params[0];
-      if(notificationType === 'Chat') {
+      if(notificationType === CHAT_NOTIFICATION_TYPE) {
         const action = params[2];
         const eventAndRoomId = decodeURIComponent(params[1]).split('|');
         const eventId = eventAndRoomId[0];
@@ -17,10 +18,18 @@ self.addEventListener('push', event => {
                 method: 'PUT',
                 credentials: 'include',
                 body: accessToken
-              }).then(resp => resp.ok && resp.json());
+              }).then(resp => {
+                if(resp?.ok) {
+                  return resp.json()
+                } else {
+                  console.error('could not retrieve the related chat event', resp);
+                  return null;
+                }
+              });
               if (chatNotification) {
                 const title = chatNotification.title;
                 chatNotification.icon = self.location.origin + chatNotification.icon;
+                chatNotification.type = CHAT_NOTIFICATION_TYPE;
                 chatNotification = prepareNotificationToSend(eventId, chatNotification);
                 await self.registration.showNotification(title, chatNotification);
                 await refreshBadge();
