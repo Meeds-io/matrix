7089420a18c1d008578a65914d6d4201f797a154
Ali HAMDI <ahamdi@exoplatform.com>
2025-08-11 17:23:03 +0100

    fix: update push notif message and fix labels - MEED-9271 - Meeds-io/MIPs#189 (#299)

--- webapp/src/main/webapp/vue-apps/notification/components/MatrixChatMentionWebPlugin.vue
+++ webapp/src/main/webapp/vue-apps/notification/components/MatrixChatMentionWebPlugin.vue
@@ -25,10 +25,16 @@ export default {
       const creator = this.notification?.parameters?.MATRIX_SENDER_FULL_NAME;
       const room = this.notification?.parameters?.MATRIX_ROOM_NAME;
 
-      return this.$t('matrix.message.mention.pwa.notification', {
-        0: creator,
-        1: `<a class="space-name font-weight-bold">${room}</a>`
-      });
+      if (this.notification?.parameters?.MATRIX_ROOM_TYPE === 'SPACE') {
+        return this.$t('matrix.message.mention.space.notification', {
+          0: `<a class="space-name font-weight-bold">${creator}</a>`,
+          1: `<a class="space-name font-weight-bold">${room}</a>`
+        });
+      } else {
+        return this.$t('matrix.message.mention.onetoone.notification', {
+          0: `<a class="space-name font-weight-bold">${room}</a>`
+        });
+      }
     }
   }
 };
