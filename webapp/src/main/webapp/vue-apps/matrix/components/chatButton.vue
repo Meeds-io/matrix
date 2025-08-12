0a4f8266fe09653b1becb22a78feea0e98f64959
Ali HAMDI <ahamdi@exoplatform.com>
2025-08-12 17:34:42 +0100

    feat: add sound effect for received messages - MEED-9268 - Meeds-io/MIPs#189 (#301)
    This will after a first message ask the user if he wants to have a bip sound when a new message is received in one of his rooms.
    If the user accepts, the next messages will be followed by a sound notification.

--- webapp/src/main/webapp/vue-apps/matrix/components/chatButton.vue
+++ webapp/src/main/webapp/vue-apps/matrix/components/chatButton.vue
@@ -44,6 +44,13 @@
     <space-form-drawer />
     <room-action-menu-drawer />
     <message-read-receipt-list-drawer />
+    <audio
+      ref="messageAudio"
+      class="hidden">
+      <source src="/matrix/audio/notif.wav">
+      <source src="/matrix/audio/notif.mp3">
+      <source src="/chat/audio/notif.ogg">
+    </audio>
   </v-app>
 </template>
 <script>
@@ -174,6 +181,7 @@
         }
       },
       enqueueMessageReceivedEvent(event) {
+        this.enableAndPlayBipSound(event);
         this.messageEventQueue.push(event);
         this.processNextMessageEvent();
       },
@@ -192,6 +200,31 @@
         } finally {
           this.processingMessageQueue = false;
           await this.processNextMessageEvent();
+        }
+      },
+      enableAndPlayBipSound() {
+        const keyToCheck = 'matrix_allow_bip';
+        if (event.detail?.message?.sender !== matrixUserId) {
+          if (localStorage.getItem(keyToCheck) === null) {
+            document.dispatchEvent(new CustomEvent('alert-message', {detail: {
+              alertType: 'info',
+              alertMessage: this.$t('matrix.message.allow.bip.ask'),
+              alertTimeout: 5000000,
+              alertLinkCallback: () =>
+              {
+                localStorage.setItem(keyToCheck, 'true');
+                document.dispatchEvent(new CustomEvent('close-alert-message'));
+              },
+              alertLinkTooltip: this.$t('matrix.message.allow.bip.confirm'),
+              alertLinkText: this.$t('matrix.message.allow.bip.link.text'),
+              alertDismissCallback: () => localStorage.setItem(keyToCheck, 'false')
+            }}));
+          } else if (localStorage.getItem(keyToCheck) === 'true') {
+            const roomIndex = this.rooms?.findIndex(room => room.id === event?.detail?.roomId);
+            if (Number.isInteger(roomIndex) && !this.rooms[roomIndex].muted) {
+              this.$refs.messageAudio.play();
+            }
+          }
         }
       },
       openDrawer() {
