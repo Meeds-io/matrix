ce26053dade1081e8dd049bcba25f181a1ec353d
Ali HAMDI <ahamdi@exoplatform.com>
2025-10-07 15:39:21 +0100

    fix: improve displaying rooms on first display - MEED-9326 (#358)
    To improve the rendering time for rooms inside the drawer :
    
     - Make the calls of loading last message and the display of the room information inside the next rendering cycle (nextTick)
     - limit the number of the initial displayed room to 30
     - The rest of roomw will be displayed if the user scrolls to the bottom and tries to display the rest of rooms.

--- webapp/src/main/webapp/vue-apps/matrix/components/MatrixChatRooms.vue
+++ webapp/src/main/webapp/vue-apps/matrix/components/MatrixChatRooms.vue
@@ -1,12 +1,40 @@
+<!--
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2025 Meeds Association contact@meeds.io
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+
 <template>
   <div
     v-if="rooms?.length"
     class="d-flex flex-column">
-    <matrix-chat-room
-      v-for="room in rooms"
-      :key="room.id"
-      :selectedRoom="selectedRoom"
-      :room="room" />
+    <div id="initialRoomsElement">
+      <matrix-chat-room
+        v-for="room in initialRooms"
+        :key="room.id"
+        :selectedRoom="selectedRoom"
+        :room="room" />
+    </div>
+    <div id="remainingRoomsElement"
+      v-intersect="onIntersect">
+      <matrix-chat-room
+        v-if="rooms?.length > limit && displayRemainingRooms"
+        v-for="room in remainingRooms"
+        :key="room.id"
+        :selectedRoom="selectedRoom"
+        :room="room" />
+    </div>
   </div>
   <div v-else-if="!loading" class="d-flex full-height align-center justify-center full-width">
     <div>
@@ -35,11 +63,25 @@
         default: null
       }
     },
+    data() {
+      return {
+        limit: 20,
+        displayRemainingRooms: false,
+      }
+    },
     created() {
       document.addEventListener('matrix-joined-room', this.addJoinedRoom);
     },
     beforeDestroy() {
       document.removeEventListener('matrix-joined-room', this.addJoinedRoom);
+    },
+    computed: {
+      initialRooms() {
+        return this.rooms.slice(0, this.limit);
+      },
+      remainingRooms() {
+        return this.rooms.slice(this.limit, this.rooms.length);
+      }
     },
     methods: {
       addJoinedRoom(event) {
@@ -52,6 +94,13 @@
           this.rooms[roomExistsIndex].members.unshift(event.detail.members);
         }
       },
+      onIntersect(entries) {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            this.displayRemainingRooms = true;
+          }
+        });
+      }
     }
   }
 </script>
