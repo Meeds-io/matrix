a6d9c2de93f186bfa8917d12f2703abd516d5faa
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2025-10-14 12:50:20 +0100

    fix: Mark own message as read after being sent - MEED-9774 - Meeds-io/meeds#3711 (#374)
    Mark own message as read after being sent

--- webapp/src/main/webapp/vue-apps/matrix/components/message/MessageComposer.vue
+++ webapp/src/main/webapp/vue-apps/matrix/components/message/MessageComposer.vue
@@ -210,7 +210,7 @@ export default {
       selection.removeAllRanges();
       selection.addRange(range);
     },
-    sendMessage() {
+    async sendMessage() {
       const composer = this.$refs.messageComposerArea;
       const messageText = composer.innerText.trim();
 
@@ -236,12 +236,12 @@ export default {
 
         message.format = "org.matrix.custom.html";
         message.formatted_body = messageHTML;
-        message['m.mentions'] = { user_ids: mentionsArray };
+        message['m.mentions'] = {user_ids: mentionsArray};
       }
 
       if (this.targetReplyMessage) {
         message['m.relates_to'] = {
-          'm.in_reply_to': { event_id: this.targetReplyMessage.event_id }
+          'm.in_reply_to': {event_id: this.targetReplyMessage.event_id}
         };
       }
 
@@ -261,7 +261,8 @@ export default {
         };
       }
 
-      this.$matrixService.sendMessage(message, this.room.id);
+      const eventId = await this.$matrixService.sendMessage(message, this.room.id);
+      this.$matrixService.markMessageAsRead(this.room.id, eventId);
       if (!this.messageToEdit) {
         this.$root.$emit('message-sent-statistics', message, this.room);
       }
