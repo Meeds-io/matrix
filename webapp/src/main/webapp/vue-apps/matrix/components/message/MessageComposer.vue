e932d5e070b209372ccf8e7bfd5acd5ad96910d4
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2025-10-16 11:27:00 +0100

    fix: Mark room as read when open conversation - MEED-9458 - Meeds-io/meeds#3705 (#378)
    Mark room as read when open conversation

--- webapp/src/main/webapp/vue-apps/matrix/components/message/MessageComposer.vue
+++ webapp/src/main/webapp/vue-apps/matrix/components/message/MessageComposer.vue
@@ -509,9 +509,6 @@ export default {
       this.$emit('input-focus', this.isInputFocused);
     },
     onInputFocus(event) {
-      setTimeout(() => {
-        this.markRoomAsRead();
-      }, 500);
       this.isInputFocused = true;
       this.$emit('input-focus', this.isInputFocused);
       this.resizeComposerArea(event);
