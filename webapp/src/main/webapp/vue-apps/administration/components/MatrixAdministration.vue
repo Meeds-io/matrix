33a48d4bf8ac3a60e51bec9b1c31b0aae4c2ac16
Ali HAMDI <ahamdi@exoplatform.com>
2026-05-22 15:18:52 +0100

    feat: enable/disable chat from administration - Meeds-io/Mips#249 - MEED-10416 (#500)
    Enable / disable chat feature from the administration

---
+++ webapp/src/main/webapp/vue-apps/administration/components/MatrixAdministration.vue
@@ -0,0 +1,53 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2026 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+<template>
+  <v-app role="main" flat>
+    <v-main class="application-body pa-5">
+      <div class="mb-5 text-title d-flex">
+        <span class="">{{ $t('meeds.chat.enabled.title') }}</span>
+        <v-switch
+          v-model="chatEnabled"
+          :ripple="false"
+          color="primary"
+          class="pa-0 my-auto ml-auto"
+          hide-details
+          @change="enableChat"/>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+<script>
+export default {
+  props: {
+  },
+  data: () => ({
+    chatEnabled: true,
+  }),
+  created() {
+    this.$matrixAdministrationService.isChatEnabled().then(respJson => {
+      this.chatEnabled = respJson.enabled;
+    });
+  },
+  methods: {
+    enableChat() {
+      this.$matrixAdministrationService.enableChatFeature(this.chatEnabled);
+    }
+  },
+};
+</script>
---
+++ webapp/src/main/webapp/vue-apps/administration/main.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2026 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+import './initComponents.js';
+import * as matrixAdministrationService from './js/matrixAdministrationService.js';
+
+// Override components if exists
+if (extensionRegistry) {
+  const components = extensionRegistry.loadComponents('matrix-administration');
+  if (components && components.length > 0) {
+    components.forEach(cmp => {
+      Vue.component(cmp.componentName, cmp.componentOptions);
+    });
+  }
+}
+
+window.Object.defineProperty(Vue.prototype, '$matrixAdministrationService', {
+  value: matrixAdministrationService,
+});
+
+const appId = 'matrixAdministrationApp';
+
+//getting language of the PLF
+const lang = eXo?.env?.portal?.language || 'en';
+
+//should expose the locale ressources as REST API
+const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.matrixAdministration-${lang}.json`;
+
+export function init() {
+  console.log('initiation of the application !');
+  exoi18n.loadLanguageAsync(lang, url).then(i18n => {
+    // init Vue app when locale resources are ready
+    Vue.createApp({
+      template: `<matrix-administration id="${appId}" />`,
+      vuetify: Vue.prototype.vuetifyOptions,
+      i18n
+    }, `#${appId}`, 'Matrix Chat Administration');
+  });
+}
