9ddcbc2c1f2df65904a07ccf559aed5af2b802c8
Ali HAMDI <ahamdi@exoplatform.com>
2026-05-22 15:18:52 +0100

    feat: enable/disable chat from administration - Meeds-io/Mips#249 - MEED-10416 (#500)
    Enable / disable chat feature from the administration

--- webapp/src/main/webapp/vue-apps/administration/initComponents.js
+++ webapp/src/main/webapp/vue-apps/administration/initComponents.js
@@ -1,8 +1,8 @@
 /*
  * This file is part of the Meeds project (https://meeds.io/).
- * 
- * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
- * 
+ *
+ * Copyright (C) 2020 - 2026 Meeds Association contact@meeds.io
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -11,16 +11,19 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-if (matrixUserId) {
-  extensionRegistry.registerComponent('UserSettingsNotifications', 'user-settings-notifications-extension', {
-    id: 'PushNotificationsSettingsExtension',
-    rank: 10,
-    vueComponent: Vue.options.components['matrix-push-notifications-settings'],
-  });
+import MatrixAdministration from './components/MatrixAdministration.vue';
+
+
+const components = {
+  'matrix-administration': MatrixAdministration,
+};
+
+for (const key in components) {
+  Vue.component(key, components[key]);
 }
