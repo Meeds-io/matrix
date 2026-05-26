abaa8afd219a4b0f9ea10852e9d6a4ca89761130
Ali HAMDI <ahamdi@exoplatform.com>
2026-05-26 12:12:19 +0100

    feat: enable deactivating private rooms from administration - MEED-10417 (#502)
    Added the possibility to deactivate creating and displaying private
    rooms between users.

---
+++ services/src/main/java/io/meeds/chat/rest/model/ChatSettings.java
@@ -0,0 +1,60 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.chat.rest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.exoplatform.ws.frameworks.json.impl.*;
+
+import java.io.ByteArrayInputStream;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatSettings {
+  private boolean chatEnabled;
+
+  private boolean privateRoomsEnabled;
+
+  private boolean spaceRoomsEnabled;
+
+  @Override
+  public String toString() {
+    try {
+      return new JsonGeneratorImpl().createJsonObject(this).toString();
+    } catch (JsonException e) {
+      throw new IllegalStateException("Error parsing current global object to string", e);
+    }
+  }
+
+  public static ChatSettings fromString(String value) {
+    if (StringUtils.isBlank(value)) {
+      return null;
+    }
+    try {
+      JsonDefaultHandler jsonDefaultHandler = new JsonDefaultHandler();
+      new JsonParserImpl().parse(new ByteArrayInputStream(value.getBytes()), jsonDefaultHandler);
+      return ObjectBuilder.createObject(ChatSettings.class, jsonDefaultHandler.getJsonObject());
+    } catch (JsonException e) {
+      throw new IllegalStateException("Error creating object from string : " + value, e);
+    }
+  }
+}
