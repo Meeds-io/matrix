5384b8810cb2d23f6293484dcf68443555918267
Ali HAMDI <ahamdi@exoplatform.com>
2026-06-24 00:11:31 +0100

    fix: update of space template setting - Meeds-io/Mips#249 - MEED-10485 (#518)

--- services/src/main/java/io/meeds/chat/rest/ChatAdministrationRest.java
+++ services/src/main/java/io/meeds/chat/rest/ChatAdministrationRest.java
@@ -3,6 +3,9 @@ package io.meeds.chat.rest;
 import io.meeds.chat.service.model.ChatSettingsEntity;
 import io.meeds.chat.service.model.ChatSettings;
 import io.meeds.chat.service.MatrixService;
+import io.meeds.chat.service.model.SpaceTemplateSetting;
+import io.meeds.social.space.template.model.SpaceTemplate;
+import io.meeds.social.space.template.model.SpaceTemplateFilter;
 import io.meeds.social.space.template.service.SpaceTemplateService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -10,13 +13,22 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import org.exoplatform.commons.exception.ObjectNotFoundException;
 import org.exoplatform.services.rest.resource.ResourceContainer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.meeds.chat.service.utils.MatrixConstants.SPACE_CHAT_AUTHORIZED;
+import static io.meeds.chat.service.utils.MatrixConstants.SPACE_CHAT_ENABLED_BY_DEFAULT;
 
 @RestController
 @RequestMapping("/chatAdministration")
@@ -57,6 +69,29 @@ public class ChatAdministrationRest implements ResourceContainer {
     this.matrixService.saveChatSettings(new ChatSettings(chatSettingsEntity.isChatEnabled(),
                                                          chatSettingsEntity.isPrivateRoomsEnabled(),
                                                          chatSettingsEntity.isSpaceRoomsEnabled()));
+    // Save changes to the space templates
+    SpaceTemplateFilter spaceTemplateFilter = new SpaceTemplateFilter(request.getRemoteUser(), request.getLocale(), false);
+    List<SpaceTemplate> spaceTemplates = spaceTemplateService.getSpaceTemplates(spaceTemplateFilter, Pageable.unpaged(), true);
+    for (SpaceTemplate spaceTemplate : spaceTemplates) {
+      SpaceTemplateSetting spaceTemplateSetting = chatSettingsEntity.getSpaceTemplateSetting()
+                                                                    .stream()
+                                                                    .filter(setting -> setting.getId() == spaceTemplate.getId())
+                                                                    .findAny()
+                                                                    .orElse(null);
+      if (spaceTemplateSetting != null) {
+        Map<String, String> extendedProperties = new HashMap<>();
+        extendedProperties.put(SPACE_CHAT_AUTHORIZED, String.valueOf(spaceTemplateSetting.isAuthorized()));
+        extendedProperties.put(SPACE_CHAT_ENABLED_BY_DEFAULT, String.valueOf(spaceTemplateSetting.isChatEnabledByDefault()));
+
+        spaceTemplate.setExtendedProperties(extendedProperties);
+        try {
+          spaceTemplateService.updateSpaceTemplate(spaceTemplate);
+        } catch (ObjectNotFoundException e) {
+          throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not find and update the space template: " + spaceTemplate.getName());
+        }
+      }
+    }
+
     return this.matrixService.loadChatSettings(request.getRemoteUser(), request.getLocale());
   }
 }
