3efc92513fa851e35456b5f087e701221c233d9f
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2026-08-12 15:21:53 +0100

    feat: Implement Chat App Center Quick Action - EXO-89179 This change will implement a new Chat app center quick action drawer and its associated BadgePlugin.

---
+++ webapp/src/main/webapp/vue-apps/quick-actions/extensions.js
@@ -0,0 +1,44 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2026 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import {ensureChatApp} from './js/chatApp.js';
+
+/**
+ * Renders the chat unread counter on the Chat tile in the Application Center.
+ * <p>
+ * `badgeName` is what App Center matches on to know this badge has a custom
+ * rendering: without it, every other application would fall back to its own
+ * default rendering being replaced.
+ */
+extensionRegistry.registerComponent('AppCenterAppBadge', 'badge', {
+  id: 'chatUnreadBadge',
+  badgeName: 'chatUnread',
+  vueComponent: Vue.options.components['matrix-chat-app-badge'],
+  rank: 10,
+  isEnabled: params => params?.badgeName === 'chatUnread',
+});
+
+extensionRegistry.registerExtension('QuickAction', 'Extension', {
+  id: 'chat',
+  icon: 'fa-comments',
+  name: 'quickActions.chat.name',
+  description: 'quickActions.chat.description',
+  click: () => ensureChatApp()
+    .then(() => document.dispatchEvent(new CustomEvent('meeds-chat-open-drawer'))),
+});
